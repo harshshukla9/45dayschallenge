03670a506adf57323ef7bb885f0135b4f59092e9
harshshukla9 <harshshukla9792@gmail.com>
2024-07-15 23:52:57 +0530

    Day-1

--- Day-1/problem1.cpp
+++ Day-1/problem1.cpp
@@ -1,1 +1,29 @@
-#include<iostream>
+// 287. Find the Duplicate Number
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+    int n= nums.size();
+
+    vector<int>v(n,-1);
+
+    for(int i=0;i<n;i++)
+    {
+        if(v[nums[i]]==-1)
+        {
+            v[nums[i]]=nums[i];
+        }
+        else
+        {
+            return nums[i];
+        }
+    }
+    return 0;
+
+    
+
+    
+        
+    }
+};
--- Day-1/problem2.cpp
+++ Day-1/problem2.cpp
@@ -1,1 +1,34 @@
-using namespace std
+// 75. Sort Colors
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        int zero=0;
+        int one=0;
+        int two=nums.size()-1;
+
+        while(one<=two)
+        {
+            if(nums[one]==0)
+            {
+                swap(nums[one],nums[zero]);
+                
+                    zero++;
+                    one++;
+                
+            }
+            else if(nums[one]==1)
+            {
+                one++;
+            }
+            else
+            {
+                swap(nums[one],nums[two]);
+                two--;
+            }
+        }
+
+
+        
+    }
+};
